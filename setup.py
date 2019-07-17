3da94149cea974d826373a2d1851e373c4202895
Hiroyuki Ohsaki <ohsaki@lsnl.jp>
2019-07-17 15:34:23 +0900

    version 1.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='xpymon',
-    version='1.5',
+    version='1.6',
     author='Hiroyuki Ohsaki',
     author_email='ohsaki@lsnl.jp',
     description='A versatile WiFi/network/battery/CPU/video system monitor on Linux',
